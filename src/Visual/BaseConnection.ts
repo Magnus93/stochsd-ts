363c7e71ac1c27f017bfa0e419e3c0c598f9af91
Magnus93 <magnus.ja.gustafsson@gmail.com>
2024-07-17 19:50:51 +0200

    Add BuildConnection

---
+++ src/Visual/BaseConnection.ts
@@ -0,0 +1,86 @@
+import { Flow, Link } from "simulation/src/api/Blocks";
+import { Engine } from "../Engine.ts";
+import { BaseVisual } from "./BaseVisual";
+import { TwoPointer } from "./TwoPointer";
+
+export class BaseConnection extends TwoPointer {
+  positionUpdateHandler: () => void
+  constructor(public id: string, public type: string, pos0: [number, number], pos1: [number, number]) {
+    super(id, type, pos0, pos1);
+    this.positionUpdateHandler = () => { // TODO fix setSourcePosition
+      // const primitive = Engine.findById(this.id)
+      // const sourcePoint = getSourcePosition(primitive)
+      // const targetPoint = getTargetPosition(primitive)
+      // this.start_anchor.setPos(sourcePoint);
+      // this.end_anchor.setPos(targetPoint);
+      // alert("Position got updated");
+    }
+  }
+
+  isAcceptableStartAttach(attachVisual: BaseVisual) {
+    // function to decide if attachVisual is OK allowed to attach start to 
+    return false;
+  }
+  isAcceptableEndAttach(attachVisual: BaseVisual) {
+    return false;
+  }
+  #startAttach?: BaseVisual
+  setStartAttach(newStartAttach: BaseVisual) {
+    if (newStartAttach != null && this.getEndAttach() == newStartAttach) {
+      return;		// Will not attach if other anchor is attached to same
+    }
+    if (newStartAttach != null && this.isAcceptableStartAttach(newStartAttach) === false) {
+      return; 	// Will not attach if not acceptable attachType
+    }
+
+    // Update the attachment primitive
+    this.#startAttach = newStartAttach;
+
+    let sourcePrimitive = null;
+    if (this.#startAttach != null) {
+      sourcePrimitive = Engine.findById(this.#startAttach.id)
+    }
+    (this.primitive as Flow | Link).start = sourcePrimitive
+
+    // Trigger the attach event on the new attachment primitives
+    this.triggerAttachEvents();
+  }
+  getStartAttach() {
+    return this.#startAttach;
+  }
+  #endAttach?: BaseVisual
+  setEndAttach(newEndAttach: BaseVisual) {
+    if (newEndAttach != null && this.getStartAttach() == newEndAttach) {
+      return; 	// Will not attach if other anchor is attached to same 
+    }
+    if (newEndAttach != null && this.isAcceptableEndAttach(newEndAttach) === false) {
+      return;		// Will not attach if not acceptable attachType
+    }
+
+    // Update the attachment primitive
+    this.#endAttach = newEndAttach;
+    let targetPrimitive = null;
+    if (this.#endAttach != null) {
+      targetPrimitive = Engine.findById(this.#endAttach.id);
+    }
+    (this.primitive as Flow | Link).end = targetPrimitive
+
+    // Trigger the attach event on the new attachment primitives
+    this.triggerAttachEvents();
+  }
+  getEndAttach() {
+    return this.#endAttach;
+  }
+  triggerAttachEvents() {
+    // We must always trigger both start and end, since a change in the start might affect the logics of the primitive attach at the end of a link or flow
+    this.getStartAttach()?.attachEvent();
+    this.getEndAttach()?.attachEvent();
+  }
+  clean() {
+    this.triggerAttachEvents();
+    super.clean();
+  }
+  updateGraphics() {
+
+  }
+}
--- src/utility.ts
+++ src/utility.ts
@@ -1,15 +1,57 @@
-import { FunctionCategories } from "./FunctionCategories";
+import { Engine } from "./Engine.ts"
+import { FunctionCategories } from "./FunctionCategories"
+import { VisualController } from "./VisualController"
 
 export function hasRandomFunction(definition: string) {
   if (definition) {
-    let randomFunctions: string[] = [];
+    let randomFunctions: string[] = []
     for (let category of FunctionCategories) {
       if (category.name === "Random Number Functions") {
-        randomFunctions = (category.functions).map(f => f.replacement.substring(0, f.replacement.indexOf("#")).toLowerCase());
-        break;
+        randomFunctions = (category.functions).map(f => f.replacement.substring(0, f.replacement.indexOf("#")).toLowerCase())
+        break
       }
     }
-    return randomFunctions.some(elem => definition.toLowerCase().includes(elem));
+    return randomFunctions.some(elem => definition.toLowerCase().includes(elem))
   }
-  return false;
+  return false
+}
+
+export function deletePrimitive(id: string) {
+  let primitive = Engine.findById(id)
+	
+  primitive.delete()
+	
+	// Delete ghosts
+	let ghostIDs = Engine.findGhostsOfPrimitive(id)
+	for(let i in ghostIDs) {
+		deletePrimitive(ghostIDs[i])
+	}
+	cleanUnconnectedLinks()
+	detachFlows(id)
+	// RunResults.removeResultsForId(id) // TODO 
+}
+
+function cleanUnconnectedLinks() {
+	let allLinks = Engine.model.findLinks()
+	for(let link of allLinks) {
+		if (!link.start || !link.end) {
+			link.delete()
+		}
+	}
+}
+
+function detachFlows(id: string) {
+	for (let key in VisualController.twoPointers) {
+		let connection = VisualController.twoPointers[key]
+		if (connection.type == "flow") { // TODO connection instanceof FlowVisual
+			// if (connection.getStartAttach() && connection.getStartAttach().id == id) {
+			// 	connection.setStartAttach(null);
+			// 	connection.update();
+			// }
+			// if (connection.getEndAttach() && connection.getEndAttach().id == id) {
+			// 	connection.setEndAttach(null);
+			// 	connection.update();
+			// }
+		}
+	}
 }
